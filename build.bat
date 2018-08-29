17663098e1f12a003df5d744cea428a924caa9e5
PKUJohnson <14203244@qq.com>
2018-08-29 21:57:05 +0800

    init version

---
+++ build.bat
@@ -0,0 +1,8 @@
+del /f/s/q dist
+del /f/s/q build
+del /f/s/q finxos.egg-info
+git pull
+python setup.py sdist
+python setup.py bdist_wheel
+twine upload dist/*
+pause
---
+++ finxos/__init__.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+"""
+    finxOS DataApi
+    ~~~~
+    Open source data tools
+    copyright: (c) 2018 finxOS.org
+    license: Apache 2.0, see LICENSE for details.
+"""
+
+import os
+
+__version__ = '0.1.0'
+SOURCE_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
